41bb725da9f27ce1828e4164ef0dea41d89e99a4
Tyson Thurman <tathurma@gmail.com>
2022-08-17 20:48:06 -0700

    simple  menu added that loops

--- main.cpp
+++ main.cpp
@@ -1,28 +1,20 @@
 #include <iostream>
 #include "SessionLog.h"
 #include <fstream>
+#include <iomanip>
 
 SessionLog create_log(std::string title, std::string notes);
 void display_log(SessionLog log);
+void print_menu();
+bool add_session_log();
+int option{0};
 
-int main(){
-    std::string title_holder{};
-    std::string notes_holder{};
-    
-    std::cout << "Enter title: ";
-    std::getline(std::cin, title_holder);
-    std::cout << "Enter notes: ";
-    std::getline(std::cin, notes_holder);
-    
-    SessionLog sample_log = create_log(title_holder, notes_holder);
-    display_log(sample_log);
-    
-    //Write the contents to a text file
-    std::ofstream output_file {"../Session_Log.txt", std::ios::app};
-    if(!output_file){
-        std::cerr << "Error opening or creating Session_Log.txt file" << std::endl;
+int main(){    
+    while(option != 1 && option !=2 && option !=3 && option !=4){
+        print_menu();
     }
-    output_file << title_holder << "\n" << notes_holder << "\n";
+    
+    //add_session_log();
     
     return 0;
 }
@@ -39,6 +31,35 @@ void display_log(SessionLog log){
     log.display_timestamp();
     std::cout << "Log Notes: " << log.get_notes() << std::endl << std::endl;
     std::cout << std::endl;
+}
+
+void print_menu(){
+    //this option is *nix specific and will not work in windows
+    system("clear");
+    std::cout << "----------Workout Logger----------" << std::endl << std::endl;
+    std::cout <<  "1.) Add Session Log\n2.) View Session Logs\n3.) Delete Session Log\n4.) Exit Program" << std::endl << std::endl;
+    std::cout << "Please select an option: " << std::endl;
+    std::cin >> option;
+}
+
+bool add_session_log() {
+    std::string title_holder{};
+    std::string notes_holder{};
+    
+    std::cout << "Enter title: ";
+    std::getline(std::cin, title_holder);
+    std::cout << "Enter notes: ";
+    std::getline(std::cin, notes_holder);
+    
+    SessionLog new_log = create_log(title_holder, notes_holder);
+    display_log(new_log);
+    
+    std::ofstream output_file {"../Session_Log.txt", std::ios::app};
+    if(!output_file){
+        std::cerr << "Error opening or creating Session_Log.txt file" << std::endl;
+    }
+    output_file << title_holder << "\n" << notes_holder << "\n";
+    return true;
 }
 
 
