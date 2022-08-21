ad04d0514f8bcb2535b2c8ee873b2b4ad7dc0610
Tyson Thurman <tathurma@gmail.com>
2022-08-21 01:38:22 -0700

    Additional fixes

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ SessionLog create_log(std::string title, std::string notes);
 void display_log(SessionLog log);
 void print_menu();
 bool add_session_log();
+void read_session_logs();
 void init();
 char option;
 
@@ -28,6 +29,8 @@ void init(){
           init();
           break;
        case '2'  :
+          read_session_logs();
+          init();
           break;
        case '3'  :
           break;
@@ -58,7 +61,7 @@ void print_menu(){
     else
         system("clear");
     std::cout << "----------Workout Logger----------" << std::endl << std::endl;
-    std::cout <<  "1.) Add Session Log\n2.) View Session Logs\n3.) Delete Session Log\n4.) Exit Program" << std::endl << std::endl;
+    std::cout <<  "1.) Add Session Log\n2.) Read Session Logs\n3.) Delete Session Log\n4.) Exit Program" << std::endl << std::endl;
     std::cout << "Please select an option: " << std::endl;
 }
 
@@ -80,5 +83,25 @@ bool add_session_log() {
         std::cerr << "Error opening or creating Session_Log.txt file" << std::endl;
     }
     output_file << "Log Date: " << converted_timestamp<< "Title: " << title_holder << "\n" << "Notes: " <<notes_holder << "\n" << "**********************\n";
+    output_file.close();
     return true;
+}
+
+void read_session_logs() {
+    std::cout << "Reading Session Logs:" << std::endl;
+    std::cout << "**********************\n\n";
+    std::string line;
+    std::ifstream input_file;
+    input_file.open("../Session_Log.txt");
+    if(!input_file){
+        std::cerr << "Error reading from Session_Log.txt file" << std::endl;
+    }
+    if (input_file.is_open()) {
+        while (std::getline(input_file, line)) {
+            std::cout << line << std::endl;
+        }
+        std::cout << "\n**********************\n";
+        input_file.close();
+        //add way to pause the window so we can see the output
+    }
 }
